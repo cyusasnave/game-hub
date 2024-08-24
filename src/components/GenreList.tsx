3d33d5f164c50e57faae970a0acd66fe74abd4e0
CYUSA Snave <cyusasnaveee@gmail.com>
2024-08-24 08:42:12 +0200

    Refactor - Simplify game query object

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -12,10 +12,10 @@ import getCroppedUrl from "../services/image-url";
 
 interface Props {
   onSelectGenre: (genre: Genre) => void;
-  selectedGenre: Genre | null;
+  selectedGenreId?: number;
 }
 
-const GenreList = ({ selectedGenre, onSelectGenre }: Props) => {
+const GenreList = ({ selectedGenreId, onSelectGenre }: Props) => {
   const { data, error, isLoading } = useGenre();
 
   if (error) return null;
@@ -42,7 +42,7 @@ const GenreList = ({ selectedGenre, onSelectGenre }: Props) => {
                   whiteSpace={"normal"}
                   textAlign={"left"}
                   fontWeight={
-                    genre.id === selectedGenre?.id ? "bold" : "normal"
+                    genre.id === selectedGenreId ? "bold" : "normal"
                   }
                   onClick={() => onSelectGenre(genre)}
                   fontSize={"lg"}
--- src/components/PlatformSelector.tsx
+++ src/components/PlatformSelector.tsx
@@ -4,13 +4,17 @@ import usePlatform, { Platform } from "../hooks/usePlatform";
 
 interface Props {
   onSelectPlatform: (platform: Platform) => void;
-  selectedPlatform: Platform | null;
+  selectedPlatformId?: number;
 }
 
-const PlatformSelector = ({ onSelectPlatform, selectedPlatform }: Props) => {
+const PlatformSelector = ({ onSelectPlatform, selectedPlatformId }: Props) => {
   const { data, error } = usePlatform();
 
   if (error) return null;
+
+  const selectedPlatform = data.results.find(
+    (platform) => platform.id === selectedPlatformId
+  );
 
   return (
     <Menu>
